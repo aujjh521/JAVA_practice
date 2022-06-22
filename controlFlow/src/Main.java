86e6aa48d5d8873c2f55eb980a835f169519e035
aujjh521 <aujjh521@gmail.com>
2022-06-22 17:58:36 +0800

    update for new code

---
+++ controlFlow/src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // 比較
+        int x = 1;
+        double y = 1D;
+        System.out.println(x == y);
+
+        // 邏輯運算 (注意& 跟 &&不一樣)
+        boolean a = true;
+        boolean b = false;
+        int x1 = 5;
+        int y1 = 0;
+        System.out.println(b && x1/y1 == 0);
+
+        //if
+        int temp = 32;
+        int high_criteria = 30;
+        int low_crteria = 20;
+        if (temp > high_criteria) {
+            System.out.println("it's a hot day");
+            System.out.println("pls drink water");
+        }
+
+        else if (temp < high_criteria && temp > low_crteria) {
+            System.out.println("it's a nice day");
+        }
+        else {
+            System.out.println("it's cold day");
+        }
+
+        //簡化的if statement
+        int income = 90_000;
+        int rich_spec = 100_000;
+        boolean hasHighIncome = (income > rich_spec);
+        System.out.println("if high income : " + hasHighIncome);
+
+
+        //ternary operator
+        int income1 = 90_000;
+        String className = income1 > 100_000 ? "First" : "Economy";
+        System.out.println("className is : " + className);
+
+
+    }
+}
